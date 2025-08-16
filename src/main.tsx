366c9e86d11e9518b8d0eb5a7caa03228d477cf7
lnxpsyhi <rommelmatic21@gmail.com>
2025-08-16 11:12:35 +0800

    improve ui

--- src/main.tsx
+++ src/main.tsx
@@ -10,16 +10,18 @@ export function Main() {
   const dispatch = useAppDispatch()
 
   return (
-    <>
+    <div className='grid grid-rows-2 grid-cols-2 gap-2'>
       <Toaster richColors position='top-center' theme='system' />
-      <h1 className='text-5xl text-purple-600'>Hello, World!</h1>
-      <Button onClick={() => {
-        dispatch(increment())
-        toast('Counter', {
-          description: `${count}`,
-        })
-      }}>Click me</Button>
+      <h1 className='text-6xl col-span-2 bg-clip-text font-extrabold text-transparent bg-linear-to-r from-green-500 to-purple-500'>Farm + React + Redux</h1>
+      <div className='col-span-2 flex items-center justify-center'>
+        <Button variant='outline' size='lg' onClick={() => {
+          dispatch(increment())
+          toast('Counter', {
+            description: `${count}`,
+          })
+        }}>Click me!</Button>
+      </div>
 
-    </>
+    </div>
   );
 }
