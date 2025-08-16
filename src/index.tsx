8184a3deb0309e0018bc015ed89987be4e0a47f9
lnxpsyhi <rommelmatic21@gmail.com>
2025-08-16 10:52:15 +0800

    redux added

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import { createRoot } from 'react-dom/client';
 import { Main } from './main';
 // import './index.css'
 import './styles/global.css';
+import StoreProvider from './StoreProvider';
 
 
 const container = document.querySelector('#root') as Element;
 const root = createRoot(container);
 
-root.render(<Main />);
+root.render(
+    <StoreProvider>
+        <Main />
+    </StoreProvider>
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
 
 import { Button } from '@/components/ui/button';
+import { increment } from '@/lib/features/counterSlice';
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { toast, Toaster } from 'sonner';
 
 
 export function Main() {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch()
+
   return (
     <>
       <Toaster richColors position='top-center' theme='system' />
       <h1 className='text-5xl text-purple-600'>Hello, World!</h1>
-      <Button onClick={() => toast('Hi!', {
-        description: 'I am a toast.',
-      })}>Click me</Button>
+      <Button onClick={() => {
+        dispatch(increment())
+        toast('Counter', {
+          description: `${count}`,
+        })
+      }}>Click me</Button>
 
     </>
   );
